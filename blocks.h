1bba3fd51dc4e50131a2939e4cd4934228f2aef0
ttr3dp <git@ttr3dp.net>
2021-02-02 01:13:13 +0100

    Update blocks

--- blocks.h
+++ blocks.h
@@ -2,13 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/                                                                        /*Interval*/ /*Signal*/
 	{"",      "torrent",                                                                          30,          0},
-	{"🏠 ",     "df -h \"/home\" | awk ' /[0-9]/ {print $4}'",                                    60,          2},
-	{"🧠 ",     "free -h | awk '/^Mem:/ {print $3 \"/\" $2}' | sed 's/i//g'",                     30,          3},
-	{"🖥 ",     "mpstat | grep -A 5 \"%idle\" | tail -n 1 | awk -F \" \" '{print 100-$13 \"%\"}'", 30,          4},
-	{"🌡 ",     "sensors | awk '/Tctl/ {print $2}' | sed 's/+//'",                                 30,          5},
+	{"",     "df -h --output=avail \"/home\" | tail -n 1",                                       60,          1},
+	{"",     "df -h --output=avail \"/media\" | tail -n 1",                                      60,          2},
+	{" ",     "free -h | awk '/^Mem:/ {print $3 \"/\" $2}' | sed 's/i//g'",                      30,          3},
+	{" ",     "mpstat | awk '/all/ {print 100-$13 \"%\"}'",                                      30,          4},
+	{" ",     "sensors | awk '/Tctl/ {print $2}' | sed 's/+//'",                                 30,          5},
 	{"",      "volume",                                                                           0,           6},
-	{"",      "cat /sys/class/net/e*/operstate | sed \"s/up/🌐/;s/down/❌/\"",                    60,          7},
-	{"",      " date '+%b %d (%a) %H:%M'",                                                        10,          8},
+	{"",      "sed \"s/up//;s/down/❌/\" /sys/class/net/e*/operstate",                           60,          7},
+	{"",      " date '+%b %d (%a) %H:%M '",                                                       10,          8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
